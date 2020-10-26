9c45f4882d1ccb6e8f34512eb118cba531237ffd
zaynegoering <zaynegoering@gmail.com>
2020-10-26 12:17:28 -0700

    commit tree 0c9473ffe2ab7ca1b4e65245c52f73d54b1aa54d

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+require 'pry'
+
+def prime?(num)
+    if num > 1
+        numbers = (2..num-1).to_a
+        if numbers.all? {|int| num % int != 0}
+            return true   
+        else 
+            return false
+        end
+    else
+        false
+    end
+end
